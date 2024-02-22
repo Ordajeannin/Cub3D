5514fe79b4b67eef8b4f8396bbf40c2d0211a1e3
Antoine JEANNIN <ajeannin@student.42.fr>
2024-02-22 21:36:00 +0100

    clean

--- srcs/maths/find_walls.c
+++ srcs/maths/find_walls.c
@@ -6,84 +6,11 @@
 /*   By: ajeannin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 19:39:50 by ajeannin          #+#    #+#             */
-/*   Updated: 2024/02/22 21:16:19 by ajeannin         ###   ########.fr       */
+/*   Updated: 2024/02/22 21:34:48 by ajeannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-void	init_grid(t_grid *grid)
-{
-	grid->fov = 60;
-	grid->half_fov = fov/2;
-	grid->proj_plan_width = 320;
-	grid->proj_plan_height = 200;
-	grid->grid_width = 64;
-	grid->angle_incr = fov/proj_plan_width;
-	t_inter *lines = NULL;
-	t_inter *col = NULL;
-}
-
-void	init_player(t_player *player)
-{
-	player->pos_x = 0;
-	player->pos_y = 0;
-	player->orientation = 0;
-}
-
-void	init_intersection(t_inter *list)
-{
-	list->grid_x = 0;
-	list->grid_y = 0;
-	list->pos_x = 0;
-	list->pos_y = 0;
-	list->next = NULL;
-}
-
-/*
-t_inter *create_arg(int pos_x, int pos_y)
-{
-	t_inter	*new_arg;
-
-	new_arg = ft_calloc(1, sizeof(t_inter));
-	if (!new_arg)
-		return (NULL);
-	new_arg->pos_x = pos_x;
-	new_arg->pos_y = pos_y;
-	new_arg->next = NULL;
-	return (new_arg);
-}
-
-int	add_arg(t_inter **list, int pos_x, int pos_y)
-{
-	t_args	*new_arg;
-	t_args	*current;
-
-	new_arg = create_arg(pos_x, pos_y);
-	if (!new_arg)
-		return (0);
-	if (*list == NULL)
-		*list = new_arg;
-	else
-	{
-		current = *list;
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new_arg;
-	}
-	return (1);
-}
-*/
-
-double dtor(double degrees)
-{
-	return (radian * 0.0174533);
-}
-
-int ft_dist(int x1, int y1, int x2, int y2)
-{
-	return ((int)sqrt(((x2 -x1) ^ 2) + ((y2 - y1) ^ 2)));
-}
 
 //stockage int: face of the block (2 bits) << texture (6 bits) << distance (24)
 //flag == 0 if col, 1 if line
@@ -181,23 +108,6 @@ void	col_intersections(t_player *player, t_grid *grid, double angle)
 		else
 			return ;
 	}
-}
-
-unsigned int	no_fish_eye(t_game *game, unsigned int value, double angle)
-{
-	double	alpha;
-	int		distorted;
-	int		correct;
-
-	if (angle < game->player->orientation)
-		alpha = game->player->orientation - angle;
-	else
-		alpha = angle - game->player->orientation;
-	distorted = (value >> 8) & DIST_MASK;
-	correct = (int)(distorted * cos(dtor(alpha)));
-	value &= ~(DIST_MASK << 8);
-	value |= (correct & DIST_MASK) << 8;
-	return (value);
 }
 
 unsigned int proj_plan_col(t_game *game, double angle)
