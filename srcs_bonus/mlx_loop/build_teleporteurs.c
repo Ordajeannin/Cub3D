da32c6f2132fa6bf5eb084543643eb43892d97dd
Antoine JEANNIN <ajeannin@student.42.fr>
2024-04-18 20:06:08 +0200

    OMFG IT WORKS

--- srcs_bonus/mlx_loop/build_teleporteurs.c
+++ srcs_bonus/mlx_loop/build_teleporteurs.c
@@ -6,7 +6,7 @@
 /*   By: ajeannin <ajeannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:13:52 by ajeannin          #+#    #+#             */
-/*   Updated: 2024/04/18 19:26:22 by ajeannin         ###   ########.fr       */
+/*   Updated: 2024/04/18 20:03:53 by ajeannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,32 +37,37 @@ char *build_telep_str(char *str)
 
 	if (!str)
 		return (NULL);
+	printf("telep string = %s\n", str);
 	result = calloc(sizeof(char), ft_strlen(str) + 1);
 	used = calloc(sizeof(char), ft_strlen(str) + 1);
 	if (!used || !result)
 		return (NULL);
 	i = 0;
 	flag = 0;
-	while (str[i])
+	while (str[i] && str[i] != '\n')
 	{
 		if (str[i] == '[' && flag == 0)
 		{
+			printf("on ouvre [\n");
 			result[i] = str[i];
 			flag = 1;
 		}
 		else if (flag != 0 && flag < 3
 				&& !add_used(used, str[i]) && ft_isalnum(str[i]))
 		{
+			printf("on add un char\n");
 			result[i] = str[i];
 			flag++;
 		}
 		else if (str[i] == ']' && flag == 3)
 		{
+			printf("on ferme ]\n");
 			result[i] = str[i];
 			flag = 0;
 		}
 		else
 		{
+			printf("str[i] = %c  |  ", str[i]);
 			printf("Bad syntax telep : [xy][ab][wd]\neach char should be unique both in peer and map\n");
 			free(used);
 			free(result);
@@ -133,6 +138,7 @@ t_telep_info	*build_info_telep(char *name, char **map)
 	node = malloc(sizeof(t_telep_info));
 	if (!node)
 		return (NULL);
+	printf("STEP 3\n");
 	node->name_enter = name[0];
 	node->x_enter = pos_in_map(map, name[0], 'x');
 	node->y_enter = pos_in_map(map, name[0], 'y');
@@ -142,20 +148,24 @@ t_telep_info	*build_info_telep(char *name, char **map)
 	return (node);
 }
 
-void	build_struct_telep(t_telep *telep, char *name, char **map)
+t_telep	*build_struct_telep(char *name, char **map)
 {
 	t_telep	*node;
 
 	node = malloc(sizeof(t_telep));
 	if (!node)
-		return ;
+		return (NULL);
+	printf("STEP 2\n");
 	node->name = name;
 	node->peer = build_info_telep(name, map);
 	node->next = NULL;
+	return (node);
+	/*
 	if (telep)
 		telep->next = node;
 	else
 		telep = node;
+*/
 }
 
 void	build_node_telep(t_game *game, char x, char y)
@@ -164,17 +174,37 @@ void	build_node_telep(t_game *game, char x, char y)
 	t_telep	*telep;
 
 	telep = game->telep;
-	while (telep)
+	while (telep && telep->next != NULL)
 		telep = telep->next;
 	if (!is_in_map_and_floor(game->grid->map, game->floor, x)
 			|| !is_in_map_and_floor(game->grid->map, game->floor, y))
 		return ;
+	printf("STEP 1\n");
 	name = calloc(sizeof(char), 3);
 	if (!name)
 		return ;
 	name[0] = x;
 	name[1] = y;
-	build_struct_telep(telep, name, game->grid->map);
+	if (telep)
+		telep->next = build_struct_telep(name, game->grid->map);
+	else
+		telep = build_struct_telep(name, game->grid->map);
+	if (!game->telep)
+		game->telep = telep;
+	printf("name : %s\n", name);
+}
+
+void display_telep_liste(t_game *game)
+{
+	t_telep *telep;
+
+	telep = game->telep;
+	printf("\n\nHey, i want to display the list\n");
+	while (telep)
+	{
+		printf("telep->name = %s\n", telep->name);
+		telep = telep->next;
+	}
 }
 
 int	build_telep(t_game *game)
@@ -184,10 +214,12 @@ int	build_telep(t_game *game)
 	i = 0;
 	if (!game->telep_str)
 		return (0);
+	printf("\n\n");
 	while (game->telep_str[i])
 	{
 		build_node_telep(game, game->telep_str[i + 1], game->telep_str[i + 2]);
 		i += 4;
 	}
+	display_telep_liste(game);
 	return (0);
 }
--- srcs_bonus/mlx_loop/teleporteurs.c
+++ srcs_bonus/mlx_loop/teleporteurs.c
@@ -6,7 +6,7 @@
 /*   By: ajeannin <ajeannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 16:21:34 by ajeannin          #+#    #+#             */
-/*   Updated: 2024/04/18 19:20:39 by ajeannin         ###   ########.fr       */
+/*   Updated: 2024/04/18 19:40:44 by ajeannin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,8 @@ void	ft_telep(t_game *game, t_player *player)
 	c = try_get_texture(game->grid->map,
 			(int)player->pos_y >> 6, (int)player->pos_x >> 6);
 	telep = check_if_telep(game, c);
+	if (telep)
+		printf("HEY! je suis sur un teleporteur!\n");
 	if (telep && player->is_in_telep == 0)
 		apply_telep(player, telep->peer, c);
 	else if (!telep)
