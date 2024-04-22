02c0df7002f90ac0991306fd9dc6a1613f7267c7
Paul KORSAKOFF <pkorsako@paul-f3Br4s3.clusters.42paris.fr>
2024-04-22 17:32:10 +0200

    leak regler cette fois

--- srcs_bonus/teleporteurs/build_teleporteurs.c
+++ srcs_bonus/teleporteurs/build_teleporteurs.c
@@ -6,7 +6,7 @@
 /*   By: pkorsako <pkorsako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 17:13:52 by ajeannin          #+#    #+#             */
-/*   Updated: 2024/04/18 21:02:42 by pkorsako         ###   ########.fr       */
+/*   Updated: 2024/04/22 17:28:07 by pkorsako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,6 @@ int	build_telep(t_game *game)
 		build_node_telep(game, game->telep_str[i + 1], game->telep_str[i + 2]);
 		i += 4;
 	}
-	free(game->telep_str);
+	// free(game->telep_str);
 	return (0);
 }
