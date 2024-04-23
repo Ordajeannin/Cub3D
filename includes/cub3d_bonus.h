f75327ecde93cd564ef4aadd90e4fed9ae2e05ab
Paul KORSAKOFF <pkorsako@paul-f4Br7s5.clusters.42paris.fr>
2024-04-23 17:28:01 +0200

    fini secret

--- includes/cub3d_bonus.h
+++ includes/cub3d_bonus.h
@@ -6,7 +6,7 @@
 /*   By: pkorsako <pkorsako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:15:52 by pkorsako          #+#    #+#             */
-/*   Updated: 2024/04/22 21:18:03 by pkorsako         ###   ########.fr       */
+/*   Updated: 2024/04/23 17:27:38 by pkorsako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@
 //////////////////  Define Player (feel free) /////////////////////
 ///////////////////////////////////////////////////////////////////
 //SCREEN//
-# define SCREEN_WIDTH 1280//640
-# define HALF_WIDTH 640
-# define SCREEN_HEIGHT 800
+# define SCREEN_WIDTH 640
+# define HALF_WIDTH 320
+# define SCREEN_HEIGHT 400
 //SKY//
 # define SKY_WIDTH 800.0
 # define SKY_HEIGHT 600
